75bef33b717f810747828050925c005a90f215f2
unknown <spacemanspiff@lutean.com>
2018-06-26 12:04:49 -0700

    metadata

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('metadata.php') ?>
 
     <script src="https://use.fontawesome.com/c2f8b0ab87.js"></script>
     <title><?php wp_title() ?></title>
---
+++ metadata.php
@@ -0,0 +1,15 @@
+<link rel="icon" href="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-32x32.png" sizes="32x32" />
+<link rel="icon" href="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-192x192.png" sizes="192x192" />
+
+<meta name='author' content='bobandbill' />
+<meta name='designer' content='Flower_Child' />
+<meta name='contact' content='zine@safarizone.net' />
+<meta name='keywords' content='safari, zine, safari, zone, pokemon, news, comics, pokedex, fangames' />
+
+<meta property="og:title" content="Safari Zine">
+<meta property="og:description" content="marcin can filld this out">
+<meta property="og:image" content="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-192x192.png">
+<meta property="og:url" content="http://zine.safarizone.net">
+<meta name="twitter:card" content="summary_large_image">
+
+<meta property="og:site_name" content="Safari Zine">
