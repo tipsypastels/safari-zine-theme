a0c0fe9e1b3c182c1c053e23045d886ede78d219
unknown <spacemanspiff@lutean.com>
2018-06-26 04:14:08 -0700

    removed menu, redid header, cleaned up styles

--- header-navbar.php
+++ header-navbar.php
@@ -1,5 +1,5 @@
 <header id="wrapper">
-  <header id="content" class="flex one-growing-element slight-mobile-margin">
+  <header id="content" class="flex wraps one-growing-element slight-mobile-margin">
     <?php site_name(['class' => "vertically-centered"]) ?>
 
     <div id="navbar-wrapper" class="grows centered">
@@ -21,9 +21,10 @@
       ?>
     </div>
 
-    <div class="menu-opener">
+    <div class="menu-marker">
       <?php fa('bars menu-opener-icon') ?>
-      <?php include('aside.php'); ?>
     </div>
+
+    <?php include('aside.php'); ?>
   </header>
 </header>
