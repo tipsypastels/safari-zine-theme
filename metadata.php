3b77776dd52f8c9ddf27ddb0230e6fb53b3cef35
unknown <spacemanspiff@lutean.com>
2018-06-27 19:39:08 -0700

    metadata cleanup, added negatives

--- metadata.php
+++ metadata.php
@@ -1,6 +1,3 @@
-<link rel="icon" href="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-32x32.png" sizes="32x32" />
-<link rel="icon" href="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-192x192.png" sizes="192x192" />
-
 <meta name='theme-color' content='#131619'>
 
 <meta name='author' content='bobandbill' />
@@ -10,7 +7,7 @@
 
 <meta property="og:title" content="Safari Zine">
 <meta property="og:description" content="marcin can filld this out">
-<meta property="og:image" content="http://zine.safarizone.net/wp-content/uploads/2018/06/cropped-logo-192x192.png">
+<meta property="og:image" content="http://zine.safarizone.net/wp-content/uploads/2018/06/logo_thick-300x283.png">
 <meta property="og:url" content="http://zine.safarizone.net">
 <meta name="twitter:card" content="summary_large_image">
 
