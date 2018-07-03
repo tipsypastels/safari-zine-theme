e6e355b8d66301d48ca09608850df0461ca2d50e
unknown <spacemanspiff@lutean.com>
2018-07-02 20:21:16 -0700

    header is now bigger

--- header-navbar.php
+++ header-navbar.php
@@ -1,28 +1,21 @@
 <header id="wrapper">
-  <header id="content" class="flex wraps one-growing-element slight-mobile-margin slight-small-computer-margin">
-    <?php site_name(['class' => "vertically-centered"]) ?>
+  <header id="content" class="slight-mobile-margin slight-small-computer-margin">
+    <div id="header-main-bar" class="flex wraps one-growing-element">
+      <?php site_name(['class' => "vertically-centered"]) ?>
 
-    <div id="navbar-wrapper" class="grows centered">
-      <!-- <ul id="navbar" class="centered-block has-invisible-links hide-on-mobile">
-        <li><a href="#">Pokédex</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Guides</a></li>
-        <li><a href="#">Fangames</a></li>
-        <li><a href="#">Showcases</a></li>
-        <li><a href="#">Other Games</a></li>
-      </ul> -->
+      <div id="navbar-wrapper" class="grows centered">
+        <?php
+          wp_nav_menu([
+            'menu' => 'header-menu',
+            'menu_class' => 'centered-block has-invisible-links hide-on-mobile',
+            'menu_id' => 'navbar'
+          ]);
+        ?>
+      </div>
 
-      <?php
-        wp_nav_menu([
-          'menu' => 'header-menu',
-          'menu_class' => 'centered-block has-invisible-links hide-on-mobile',
-          'menu_id' => 'navbar'
-        ]);
-      ?>
-    </div>
-
-    <div class="menu-marker">
-      <?php fa('bars menu-opener-icon') ?>
+      <div class="menu-marker">
+        <?php fa('bars menu-opener-icon') ?>
+      </div>
     </div>
 
     <?php include('aside.php'); ?>
