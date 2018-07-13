5051498fa892b805aa55f8ec8101567d1d7de3a6
unknown <spacemanspiff@lutean.com>
2018-07-13 05:48:21 -0700

    converted header to sticky and used a pseudo-variable to calculate the header height

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
  jQuery(document).ready(function($) {
-   $(".menu-marker").click(() => {
-     $("header#wrapper").toggleClass("expanded");
-   });
+  $(".menu-marker").click(() => {
+    $("body").toggleClass("header-expanded");
+  });
 
-   $("#back-to-top").click(() => {
-     $("html, body").animate({ scrollTop: 0 }, 'slow');
-   });
+  $("#back-to-top").click(() => {
+    $("html, body").animate({ scrollTop: 0 }, 'slow');
+  });
+
+  $(window).scroll(() => {
+    var body   = $("body");
+    var scroll = $(window).scrollTop();
+
+    (scroll > 100) ? 
+      body.addClass('docked') : 
+      body.removeClass('docked');
+  });
 });
 
