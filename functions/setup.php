a281182ff765bc6c2d73e837e31ca5bbb7028c4d
unknown <spacemanspiff@lutean.com>
2018-08-15 15:34:06 -0700

    added a filter for the pw resets

--- functions/setup.php
+++ functions/setup.php
@@ -44,4 +44,10 @@ function filter_site_upload_size_limit($size) {
 
 add_filter('upload_size_limit', 'filter_site_upload_size_limit', 20);
 
+function fix_brackets_in_password_reset($message, $key, $user_login, $user_data) {
+  return "Someone has requested a password reset for the following account: " . sprintf(__('%s'), $user_data->user_email) . "\nIf this was a mistake, just ignore this email.\r\r\n\nTo reset your password, visit the following address: " . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . "\r\nDakota was here.";  
+}
+
+add_filter('retrieve_password_message', 'fix_brackets_in_password_reset', 99, 4);
+
 ?>
