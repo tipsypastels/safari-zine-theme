796b3ac8bb50ac1fe8f63fa6ceb9cc4071c8b94b
unknown <spacemanspiff@lutean.com>
2018-08-25 06:41:20 -0700

    fixed user bit not displaying on articles, added eyes to footer kanga

--- functions/user_blocks.php
+++ functions/user_blocks.php
@@ -1,10 +1,10 @@
 <?php
   function user_bit($username, $user_id) {
+    if (!$username) return;
+
     $description = get_userdata($user_id)->user_description;
     $avatar = get_wp_user_avatar_src($user_id);
     $user_href = get_author_posts_url($user_id); 
-
-    if (!$author) return; 
   ?>
     <a class="user-bit flex vertically-centered centered-block-on-mobile has-invisible-links" href="<?php echo $user_href ?>">
       <img class="avatar small" src="<?php echo $avatar ?>">
