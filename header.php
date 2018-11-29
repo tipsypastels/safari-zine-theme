ea509e4cac4688929fe49f6c9edffec820baec0f
tipsypastels <dakotasankey@gmail.com>
2018-11-28 22:11:15 -0800

    metadata

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <?php ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="alternate" href="http://zine.safarizone.net/" hreflang="en"/>
+    
     <script src="https://use.fontawesome.com/c2f8b0ab87.js"></script>
     <?php
       if (!function_exists('_wp_render_title_tag')) {
